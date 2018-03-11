6b2262c5b1bda425a7994d5a39f47f69ee14368c
Adam Mankowski <maniekmanfred69@gmail.com>
2018-03-11 19:37:58 +0100

    Add Eurovision project source files

---
+++ countries.js
@@ -0,0 +1,53 @@
+module.exports = function countries() {
+  return [
+    //Released
+    {county: 'Poland', ew: 'https://eurovisionworld.com/eurovision/2018/poland', yt: 'yfUJ2eDm6ng'},
+    {county: 'Estonia', ew: 'https://eurovisionworld.com/eurovision/2018/estonia', yt: '76KOUIfDry8'},
+    {county: 'Finland', ew: 'https://eurovisionworld.com/eurovision/2018/finland', yt: 'y4xlvh6qW6M'},
+    {county: 'Spain', ew: 'https://eurovisionworld.com/eurovision/2018/spain', yt: 'FDga57mNny4'},
+    {county: 'Netherlands', ew: 'https://eurovisionworld.com/eurovision/2018/netherlands', yt: 'kLL9IlQ_7OA'},
+    {county: 'Germany', ew: 'https://eurovisionworld.com/eurovision/2018/germany', yt: 'o_xTETHwIQg'},
+    {county: 'Italy', ew: 'https://eurovisionworld.com/eurovision/2018/italy', yt: 'gs2n5JY0QzQ'},
+    {county: 'France', ew: 'https://eurovisionworld.com/eurovision/2018/france', yt: 'L7dFXOyvWVE'},
+    {county: 'Ukraine', ew: 'https://eurovisionworld.com/eurovision/2018/ukraine', yt: 'E1yoAtjhkzQ'},
+    {county: 'United Kingdom', ew: 'https://eurovisionworld.com/eurovision/2018/united-kingdom', yt: 'K--kIdOpbJM'},
+    {county: 'Latvia', ew: 'https://eurovisionworld.com/eurovision/2018/latvia', yt: 'uBlZsGxeXk4'},
+    {county: 'Romania', ew: 'https://eurovisionworld.com/eurovision/2018/romania', yt: 'lZp6QwWDQ4c'},
+    {county: 'Hungary', ew: 'https://eurovisionworld.com/eurovision/2018/hungary', yt: '6unRU5ZHbqY'},
+    {county: 'Moldova', ew: 'https://eurovisionworld.com/eurovision/2018/moldova', yt: 'Kg3ecHrFM_4'},
+    {county: 'Serbia', ew: 'https://eurovisionworld.com/eurovision/2018/serbia', yt: 'WkOFnIjGrkw'},
+    {county: 'Malta', ew: 'https://eurovisionworld.com/eurovision/2018/malta', yt: 'E_0ugf0eP1Q'},
+    {county: 'Slovenia', ew: 'https://eurovisionworld.com/eurovision/2018/slovenia', yt: 'Y-Q4Fh54wUk'},
+    {county: 'Switzerland', ew: 'https://eurovisionworld.com/eurovision/2018/switzerland', yt: 'jXOlPGnkZFY'},
+    {county: 'Czech Republic', ew: 'https://eurovisionworld.com/eurovision/2018/czech-republic', yt: 'DLGfxc3eTNw'},
+    {county: 'Belarus', ew: 'https://eurovisionworld.com/eurovision/2018/belarus', yt: 'YU2My4BPX34'},
+    {county: 'Azebaijan', ew: 'https://eurovisionworld.com/eurovision/2018/azerbaijan', yt: 'EVEhrwDzdCE'},
+    {county: 'Greece', ew: 'https://eurovisionworld.com/eurovision/2018/greece', yt: 'z6hvV9-Gi_w'},
+    {county: 'Denmark', ew: 'https://eurovisionworld.com/eurovision/2018/denmark', yt: 'XeraDSzu0nw'},
+    //Not released yet
+    {county: 'San Marino', ew: 'https://eurovisionworld.com/eurovision/2018/san-marino', yt: 'ZNflc1Zcw2A'},
+    {county: 'Iceland', ew: 'https://eurovisionworld.com/eurovision/2018/iceland', yt: 'nloweviD_90'},
+    {county: 'Montenegro', ew: 'https://eurovisionworld.com/eurovision/2018/montenegro', yt: ''},
+    {county: 'Albania', ew: 'https://eurovisionworld.com/eurovision/2018/albania', yt: ''},
+    {county: 'Georgia', ew: 'https://eurovisionworld.com/eurovision/2018/georgia', yt: ''},
+    {county: 'Austria', ew: 'https://eurovisionworld.com/eurovision/2018/austria', yt: 'I8MyztgOTv8'},
+    {county: 'Armenia', ew: 'https://eurovisionworld.com/eurovision/2018/armenia', yt: ''},
+    {county: 'Cyprus', ew: 'https://eurovisionworld.com/eurovision/2018/cyprus', yt: ''},
+    {county: 'Ireland', ew: 'https://eurovisionworld.com/eurovision/2018/ireland', yt: 'XAEjQXzW4Uc'},
+    //The artist & song for Bulgaria will be announced on 12 March 2018
+    {county: 'Bulgaria', ew: '', yt: ''},
+    //The artist & song for Sweden will be decided on 10 March 2018
+    {county: 'Sweden', ew: '', yt: 'U2UmYBkszOA'},
+    //Jessica Mauboy's song for Eurovision 2018 will be released in March 2018
+    {county: 'Australia', ew: '', yt: 'J4XZxbrvepw'},
+    //Yulia Samoylova's song for Eurovision 2018 will be released in March 2018
+    {county: 'Russia', ew: '', yt: ''},
+    {county: 'Belgium', ew: '', yt: '6DGf9TMehi0'},
+    {county: 'Israel', ew: '', yt: ''},
+    {county: 'Croatia', ew: '', yt: '19Yo86KCd0w'},
+    {county: 'Norway', ew: '', yt: 'oicLsTZRp28'},
+    {county: 'Portugal', ew: '', yt: 'kaVp4El9p3s'},
+    {county: 'Lithuania', ew: '', yt: ''},
+    {county: 'FYR Macedonia', ew: '', yt: ''},
+  ];
+};
--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var countries = require('./countries.js');
+var request = require('request');
+
+function retrieveResults() {
+  let results = [];
+
+  countries().forEach((country) => {
+    if(country.yt == '')
+      return;
+
+    request('https://www.googleapis.com/youtube/v3/videos?part=statistics&id=yfUJ2eDm6ng&key=AIzaSyD3mW7_XpOdbeMt0NLRCalpqtGsgKKTVuQ', function (error, response, body) {
+      body = JSON.parse(body);
+      results.push({ views: body.items[0].statistics.viewCount, likes: body.items[0].statistics.likeCount, dislikes: body.items[0].statistics.likeCount, comments: body.items[0].statistics.commentCount});
+    });
+  });
+  return results;
+}
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,11 +27,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index')
-});
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
+  const results = retrieveResults();
+  response.render(results);
 });
 
 app.listen(app.get('port'), function() {
