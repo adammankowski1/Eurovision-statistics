0dce9b956d49ab0dfae0b3c6cf73a7c9b66f98db
Adam Mankowski <maniekmanfred69@gmail.com>
2018-03-11 21:15:03 +0100

    Basic eurovision songs statistics work

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 var express = require('express');
 var app = express();
 var countries = require('./countries.js');
-var request = require('request');
+var request = require('request-promise-native');
 
-function retrieveResults() {
+async function retrieveResults() {
   let results = [];
 
-  countries().forEach((country) => {
-    if(country.yt == '')
-      return;
+  let url = 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id=yfUJ2eDm6ng';
 
-    request('https://www.googleapis.com/youtube/v3/videos?part=statistics&id=yfUJ2eDm6ng&key=AIzaSyD3mW7_XpOdbeMt0NLRCalpqtGsgKKTVuQ', function (error, response, body) {
-      body = JSON.parse(body);
-      results.push({ views: body.items[0].statistics.viewCount, likes: body.items[0].statistics.likeCount, dislikes: body.items[0].statistics.likeCount, comments: body.items[0].statistics.commentCount});
+  countries().forEach((country, index) => {
+    if(country.yt == '') {
+      return;
+    }
+    url += country.yt;
+    if(index != 0 && index != countries().lenght - 1)
+      url += ',';
+  });
+
+  url += '&key=AIzaSyD3mW7_XpOdbeMt0NLRCalpqtGsgKKTVuQ';
+  await request(url, function (error, response, body) {
+    body = JSON.parse(body);
+    body.items.forEach((item, index) => {
+      results.push({country: countries()[index].name, views: item.statistics.viewCount, likes: item.statistics.likeCount, dislikes: item.statistics.likeCount, comments: item.statistics.commentCount});
     });
   });
   return results;
@@ -26,9 +35,9 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  const results = retrieveResults();
-  response.render(results);
+app.get('/', async function(request, response) {
+  const results = await retrieveResults();
+  response.send(results);
 });
 
 app.listen(app.get('port'), function() {
