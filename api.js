c3327fa2aaba05e84c98377a6ce3200e2e6009c2
Adam Mankowski <maniekmanfred69@gmail.com>
2018-03-13 22:28:03 +0100

    Fix time of records

--- api.js
+++ api.js
@@ -20,13 +20,11 @@ exports.retrieveResults = async function() {
   url += `&key=${process.env.youtube_api_key}`;
   await request(url, function (error, response, body) {
     body = JSON.parse(body);
+    var currentTime = new Date();
+    currentTime.setMinutes(0);
+    currentTime.setSeconds(0);
+    currentTime.setMilliseconds(0);
     body.items.forEach((item, index) => {
-      var currentTime = new Date();
-      if(index < 24)
-        currentTime.setHours(index);
-      currentTime.setMinutes(0);
-      currentTime.setSeconds(0);
-      currentTime.setMilliseconds(0);
       results.push({country: filteredCountries[index].name, views: item.statistics.viewCount, likes: item.statistics.likeCount, dislikes: item.statistics.dislikeCount, likesRatio: (item.statistics.likeCount / item.statistics.dislikeCount).toFixed(2), comments: item.statistics.commentCount, id: item.id, time: currentTime.toISOString()});
     });
   });
