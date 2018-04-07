b342abc1aa82f0eca0a4f83c763c56168b9ae996
Adam Mankowski <adammankowski97@gmail.com>
2018-04-07 13:14:38 +0200

    Add table component and data fetching for live statistics

--- src/components/liveStatistics/index.js
+++ src/components/liveStatistics/index.js
@@ -1,7 +1,53 @@
 import React from 'React';
+import { Table } from 'antd';
 
 export default class LiveStatistics extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dataSource: [],
+      columns: [{
+        title: 'Country',
+        dataIndex: 'country',
+        key: 'country',
+      }, {
+        title: 'Views',
+        dataIndex: 'views',
+        key: 'views',
+      }, {
+        title: 'Likes',
+        dataIndex: 'likes',
+        key: 'likes',
+      }, {
+        title: 'Dislikes',
+        dataIndex: 'dislikes',
+        key: 'dislikes',
+      }, {
+        title: 'Likes to dislikes ration',
+        dataIndex: '',
+        key: '',
+      }, {
+        title: 'Comments',
+        dataIndex: 'comments',
+        key: 'comments',
+    }]};
+  }
+
+  componentDidMount() {
+    fetch("/api")
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      this.setState({dataSource: data});
+    });
+  }
+
   render() {
-    return <div>Live Statistics</div>
+    return (
+      <div>
+        <div>Live Statistics</div>
+        <Table dataSource={this.state.dataSource} columns={this.state.columns} />
+      </div>
+    )
   }
 };
